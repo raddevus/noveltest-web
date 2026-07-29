4f4099e38768bc750a8b823323983d4a4c3620cf
raddevus <1radeutsch1@gmail.com>
2026-07-29 16:34:50 -0400

    Added if statement so if user forgets to add a filename they get an error msg & no file is creatd.

--- newPost.sh
+++ newPost.sh
@@ -1,1 +1,5 @@
+if [ -z "$dirPath" ]; then
+        echo "Please provide a valid base filename. .md will be added as a suffix. Exiting..."
+        exit 1
+fi
 cp src/data/blog-posts/template.0md src/data/blog-posts/$1.md
